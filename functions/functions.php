6ffc22e9878594477ff6bd5e09c852671b89d08a
MatteoTuveri <140177942+MatteoTuveri@users.noreply.github.com>
2023-11-30 17:49:58 +0100

    Correct while cicle

--- functions/functions.php
+++ functions/functions.php
@@ -4,15 +4,15 @@ function passGen($number)
     $letters = 'abcdefghijklmnopqrstuvwxyz';
     $caps = strtoupper($letters);
     $symbols = "~! @#$%^&*()_-+={[}]|\:;<,>.?/";
-    $numbers ='0123456789';
+    $numbers = '0123456789';
     $password = '';
     while (strlen($password) <= $number) {
-        $item = str_split($letters . $caps . $symbols.$numbers)[rand(0, strlen($letters . $caps . $symbols.$numbers))];
+        $item = str_split($letters . $caps . $symbols . $numbers)[rand(0, strlen($letters . $caps . $symbols . $numbers) - 1)];
         if (!str_contains($password, $item)) {
-        $password .= $item;
-        }
+            $password .= $item;
+        } 
+    }
     return $password;
-}
 }
 ;
 ?>
